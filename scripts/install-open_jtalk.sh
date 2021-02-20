9715d88d072b1f8d8852a54bb147834520c5318c
hrhr49 <hiro49410@gmail.com>
2021-02-20 16:00:40 +0900

    open_jtalkのビルドスクリプトのバグ修正

--- scripts/install-open_jtalk.sh
+++ scripts/install-open_jtalk.sh
@@ -7,6 +7,7 @@ TOOL_DIR="$HERE/../tools"
 JS_DIR="$HERE/../js"
 EMSDK_DIR="$TOOL_DIR/emsdk"
 OPEN_JTALK_DIR="$TOOL_DIR/open_jtalk"
+HTS_ENGINE_API_DIR="$TOOL_DIR/hts_engine_API"
 
 mkdir -p "$TOOL_DIR"
 mkdir -p "$JS_DIR"
@@ -26,4 +27,22 @@ emcmake cmake -DCMAKE_BUILD_TYPE=Release \
 emmake make
 
 # libopenjtalk.aからwasm及びjsファイルを作成
-emcc "$OPEN_JTALK_DIR/src/build/libopenjtalk.a" -o "$JS_DIR/open_jtalk.js"
+# src/bin/open_jtalk.cをビルド
+emcc "$OPEN_JTALK_DIR/src/bin/open_jtalk.c" \
+  "$OPEN_JTALK_DIR/src/build/libopenjtalk.a" \
+  "$HTS_ENGINE_API_DIR/src/build/lib/libhts_engine_API.a" \
+	-I $OPEN_JTALK_DIR/src/jpcommon \
+	-I $OPEN_JTALK_DIR/src/mecab/src \
+	-I $OPEN_JTALK_DIR/src/mecab2njd \
+	-I $OPEN_JTALK_DIR/src/njd \
+	-I $OPEN_JTALK_DIR/src/njd2jpcommon \
+	-I $OPEN_JTALK_DIR/src/njd_set_accent_phrase \
+	-I $OPEN_JTALK_DIR/src/njd_set_accent_type \
+	-I $OPEN_JTALK_DIR/src/njd_set_digit \
+	-I $OPEN_JTALK_DIR/src/njd_set_long_vowel \
+	-I $OPEN_JTALK_DIR/src/njd_set_pronunciation \
+	-I $OPEN_JTALK_DIR/src/njd_set_unvoiced_vowel \
+	-I $OPEN_JTALK_DIR/src/text2mecab \
+  -I $HTS_ENGINE_API_DIR/lib \
+  -I $HTS_ENGINE_API_DIR/include \
+  -o "$JS_DIR/open_jtalk.js"
