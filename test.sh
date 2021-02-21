dd269c8d445af144b52645a3f4a06dff2a4d9f18
hrhr49 <hiro49410@gmail.com>
2021-02-21 11:48:16 +0900

    LICENSEの追加。サンプル用の辞書ファイルとhtsvoiceファイルを変更。

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
-
 # node js/open_jtalk.js \
 open_jtalk.js \
--x etc/naist-jdic \
--m etc/mei_normal.htsvoice \
+-x etc/open_jtalk_dic_utf_8-1.11 \
+-m etc/mei/mei_normal.htsvoice \
 -r 1.0 \
 -ow tmp.wav \
 etc/sample.txt
